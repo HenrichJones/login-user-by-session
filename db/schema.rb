47bcfc49d3fab7a2437f1d9133c336dd390b376d
Mr Jones <jones@rogalabs.com>
2019-10-07 06:50:12 -0300

    added password digest
    Signed-off-by: Mr Jones <jones@rogalabs.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_03_154803) do
+ActiveRecord::Schema.define(version: 2019_10_07_094938) do
 
   create_table "users", force: :cascade do |t|
     t.string "fullname"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_10_03_154803) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
