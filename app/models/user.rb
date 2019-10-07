ca64b9b05a26490220b68ef71d208521f7e1a30e
Mr Jones <jones@rogalabs.com>
2019-10-07 07:05:03 -0300

    added new validation to users
    - Validate, password, email and name,
    - Added index to users email,
    - Created migration
    
    Signed-off-by: Mr Jones <jones@rogalabs.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates name,
+            presence: true,
+            length: { maximum: 50 }
+  
+  validates password,
+            presence: true,
+            length: { minimum: 8 }
+
+  validates email,
+            presence: true,
+            format: { with: URI::MailTo::EMAIL_REGEXP },
+            uniqueness: { case_sensitive: false },
+            length: { maximum: 260},
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_07_094938) do
+ActiveRecord::Schema.define(version: 2019_10_07_100120) do
 
   create_table "users", force: :cascade do |t|
     t.string "fullname"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_10_07_094938) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email"
   end
 
 end
