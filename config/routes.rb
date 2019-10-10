c98283788dd2cc1d3f06c25ab0470782c2ed8d59
Mr Jones <jones@rogalabs.com>
2019-10-09 23:05:11 -0300

    adjusts on variables name and route path name
    Signed-off-by: Mr Jones <jones@rogalabs.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :users
   root 'sessions#new'
 
-  delete 'sing_out' => 'sessions#destroy'
+  delete 'sign_out' => 'sessions#destroy'
   post   'sign_in'  => 'sessions#create'
   get    'sign_in'  => 'sessions#new'
 
