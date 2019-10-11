412039332808c95ed179ef5b9fa7bcb067cd5d00
Mr Jones <jones@rogalabs.com>
2019-10-10 22:36:44 -0300

    added method to verify if user is logged
    Signed-off-by: Mr Jones <jones@rogalabs.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  def authorize
+    unless logged_in?
+      redirect_to root_url
+    end
+  end
 end
