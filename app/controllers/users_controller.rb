07fba07dab84364d913b3d51617809bc76163f37
Mr Jones <jones@rogalabs.com>
2019-10-12 22:31:26 -0300

    added before action to users controller - Added action to view show file
    Signed-off-by: Mr Jones <jones@rogalabs.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authorize, except: [:new, :create]
+  before_action :correct_user?, only: [:edit, :update, :destroy]
 
   def index
     @users = User.all
